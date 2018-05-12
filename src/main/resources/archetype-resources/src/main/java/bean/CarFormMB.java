bc3c4b1bc2ceecb0474ad2ea31848ec46d0f84ac
rmpestano <rmpestano@gmail.com>
2018-05-11 22:05:39 -0300

    fixes after remove redirect

--- src/main/resources/archetype-resources/src/main/java/bean/CarFormMB.java
+++ src/main/resources/archetype-resources/src/main/java/bean/CarFormMB.java
@@ -69,7 +69,7 @@ public class CarFormMB implements Serializable {
             addDetailMessage("Car " + car.getModel()
                     + " removed successfully");
             Faces.getFlash().setKeepMessages(true);
-            Faces.redirect("car-list.xhtml");
+            Faces.redirect("car-list.jsf");
         }
     }
 
